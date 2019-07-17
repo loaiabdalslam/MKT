cbdb7a1655b440d89f9114255469918b7632291d
loaiabdalslam <loaiabdalslam@gmail.com>
2019-07-17 21:33:04 +0200

    start using npx standard style

---
+++ helper/util.js
@@ -0,0 +1,44 @@
+const SECS_IN_DAY = 86400
+
+function convertPriceArrayToMap (priceArray) {
+  const map = new Map()
+  for (let price of priceArray) {
+    const time = secsToDate(price.time)
+    map.set(startOfDay(time).valueOf(), Object.assign({}, price, { time }))
+  }
+  return map
+}
+
+// simple linear interpolation
+function getPriceFromOHLC (priceData, time) {
+  const { open, /* high, low, */ close } = priceData
+
+  const startTime = startOfDay(time)
+  const diffSecs = (time - startTime) / 1000
+
+  const diffPrice = close - open
+  const price = (diffPrice * diffSecs / SECS_IN_DAY) + open
+
+  return price
+}
+
+function cloneDate (date) {
+  return new Date(date.getTime())
+}
+
+function secsToDate (secs) {
+  return new Date(secs * 1000)
+}
+
+// return UTC start of day
+function startOfDay (date) {
+  let cd = cloneDate(date)
+  cd.setUTCHours(0, 0, 0, 0)
+  return cd
+}
+
+module.exports = {
+  convertPriceArrayToMap,
+  getPriceFromOHLC,
+  startOfDay
+}
--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-const { sympolPrice } = require('./price');
-const {
-	get
-} = require('./fetch');
-const link = 'https://min-api.cryptocompare.com/data/';
+const { sympolPrice } = require("./price")
+const { get } = require("./helper/fetch")
+const link = "https://min-api.cryptocompare.com/data/";
 
 module.exports = {
+  MKT: function (api) {
+    // This Function for initialize the api
+    this.apikey = () => {
+      return `apikey=${api}`
+    }
 
-	MKT: function(api) {
-		// This Function for initialize the api 
-		this.apikey = () => {
-			return `apikey=${api}`
-		}
+    // this Function for initialize the query
+    this.price = dict => {
+      return sympolPrice(dict)
+    }
 
-		// this Function for initialize the query
-		this.price = (dict) => {
-			return sympolPrice(dict)
+    // this Function fireup exchange query for make request using your api and your query and return promise reponse
+    // you can access reponse data with then((response)=>Json.stringify(response.data))
 
-		}
-
-		// this Function fireup exchange query for make request using your api and your query and return promise reponse
-		// you can access reponse data with then((response)=>Json.stringify(response.data))
-		
-		this.exchange = (dict) => {
-			let request = link + this.price(dict) + this.apikey()
-			return get(request);
-		}
-
-
-	}
-
+    this.exchange = dict => {
+      const request = link + sympolPrice(dict) + this.apikey()
+      return get(request)
+    }
+  }
 }
-
